92f654622af25362736cad0d1f9ca9541ed4cbf2
monir3430 <miamonir3430@gmail.com>
2024-10-27 20:17:05 +0600

    first

---
+++ book_store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class bookModel(models.Model):
+    type = (
+        ('Mystery', 'Mystery'),
+        ('Thriller', 'Thriller'),
+        ('Flying', 'Flying'),
+        ('Horror', 'Horror'),
+    )
+    id = models.integerField(primary_key=True)
+    book_name = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, choices=type)
+    first_publish = models.DateTimeField(auto_now_add=True)
+    last_publish = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
