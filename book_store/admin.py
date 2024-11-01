e69d0b1d2c4e7c5e9ffb34bbd54d81a39850279d
monir3430 <miamonir3430@gmail.com>
2024-11-01 21:17:44 +0600

    inheritande-abstract base and multitable

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from book_store.models import bookModel
+from book_store.models import bookModel, author, reader, people, man, women
 
 
 # Register your models here.
@@ -8,3 +8,26 @@ class bookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(bookModel, bookAdmin)
+admin.site.register(author)
+admin.site.register(reader)
+
+
+# Register People model with the admin site
+@admin.register(people)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')  # Fields to display in the list view
+    search_fields = ('name',)  # Enable search by 'name'
+
+
+# Register Man model with the admin site
+@admin.register(man)
+class ManAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'power', 'monogamy')  # Include inherited and unique fields
+    list_filter = ('power', 'monogamy')  # Add filters for 'power' and 'monogamy'
+
+
+# Register Women model with the admin site
+@admin.register(women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'married', 'unmarried')  # Include inherited and unique fields
+    list_filter = ('married', 'unmarried')  # Add filters for 'married' and 'unmarried'
--- book_store/models.py
+++ book_store/models.py
@@ -17,3 +17,37 @@ class bookModel(models.Model):
     last_publish = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.book_name
+
+
+class common(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=30)
+
+    class meta:
+        abstract = True
+
+
+class author(common):
+    address = models.CharField(max_length=100)
+
+
+class reader(common):
+    likes = models.CharField(max_length=30)
+    dislikes = models.CharField(max_length=30)
+
+
+class people(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+
+class man(people):
+    power = models.BooleanField()
+    monogamy = models.BooleanField()
+
+
+class women(people):
+    married = models.BooleanField()
+    unmarried = models.BooleanField()
